e1fe7ef34d7d98bc8992e33dcc5c95d596548d99
Jason Y <110353075+JasonY199@users.noreply.github.com>
2024-10-14 18:33:05 -0400

    Ch 2: Nodes, 4: LeafNode

--- src/textnode.py
+++ src/textnode.py
@@ -13,15 +13,15 @@ class TextType(Enum):
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
-        self.text_type = text_type
+        self.text_type = text_type.value
         self.url = url
 
     def __eq__(self, other):
         return (
-            self.text_type.value == other.text_type.value
+            self.text_type == other.text_type
             and self.text == other.text
             and self.url == other.url
         )
 
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
